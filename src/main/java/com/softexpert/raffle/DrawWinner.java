8bdae29bddfae83544bb416960b7341f189468d5
willian.cunha <willian.cunha@softexpert.com>
2016-03-08 09:09:56 -0300

    Refatoração da validação de vencedor único

--- src/main/java/com/softexpert/raffle/DrawWinner.java
+++ src/main/java/com/softexpert/raffle/DrawWinner.java
@@ -6,7 +6,11 @@ import java.util.Random;
 
 public class DrawWinner {
 
-	private Random randomGenerator = new Random();
+	private Random randomGenerator;
+	
+	public DrawWinner(Random randomGenerator){
+		this.randomGenerator = randomGenerator;
+	}
 
 	public List<String> buildWinner(List<String> participants, int numberOfWinners) {
 		List<String> winners = new ArrayList<String>();
@@ -14,21 +18,26 @@ public class DrawWinner {
 
 		while (winners.size() != numberOfWinners) {
 			winnerCandidate = getRandomWinner(participants);
-			boolean isOnTheList = false;
-			for (String winner : winners)
-				if (winner == winnerCandidate) {
-					isOnTheList = true;
-					break;
-				}
-			if (!isOnTheList)
+			if (isUnique(winners, winnerCandidate))
 				winners.add(winnerCandidate);
 		}
 		return winners;
 	}
 
-	private String getRandomWinner(List<String> participantes) {
-		int randomNumber = randomGenerator.nextInt(participantes.size());
-		return participantes.get(randomNumber);
+	private String getRandomWinner(List<String> participants) {
+		int randomNumber = randomGenerator.nextInt(participants.size());
+		return participants.get(randomNumber);
+	}
+	
+	private boolean isUnique(List<String> winners, String winnerCanditate) {
+		boolean isUnique = true;
+		
+		for (String winner : winners)
+			if (winner == winnerCanditate) {
+				isUnique = false;
+				break;
+			}
+		return isUnique;
 	}
 
 }
--- src/test/java/com/softexpert/raffle/DrawWinnerTest.java
+++ src/test/java/com/softexpert/raffle/DrawWinnerTest.java
@@ -4,27 +4,41 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
+import java.util.Random;
 
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 public class DrawWinnerTest {
 
-	private DrawWinner drawWinner = new DrawWinner();
+	@InjectMocks
+	private DrawWinner drawWinner;
+	@Mock
+	private Random randomGenerator;
+
 	private FileReader fileReader = new FileReader();
+
+	@Before
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+	}
+
 	private static final List<String> DEFAULT_NAMES_LIST = Arrays.asList("ABNER MARCOS ORLAMUNDER", "ABRAAO PUZAK",
 			"ADAEL OURIQUES DA CONCEICAO", "ADILSON JOAO REIS JUNIOR", "ADRIAN MATEUS SOUSA SANTOS",
 			"ADRIANA ALVES DO PRADO");
 
 	@Test
 	public void oneWinnerDrawingStatic() {
-		Collection<String> winners = drawWinner.buildWinner(DEFAULT_NAMES_LIST, 1);
-		MatcherAssert.assertThat(winners,
-				Matchers.anyOf(Matchers.hasItem("ABNER MARCOS ORLAMUNDER"), Matchers.hasItem("ABRAAO PUZAK"),
-						Matchers.hasItem("ADAEL OURIQUES DA CONCEICAO"), Matchers.hasItem("ADILSON JOAO REIS JUNIOR"),
-						Matchers.hasItem("ADRIAN MATEUS SOUSA SANTOS"), Matchers.hasItem("ADRIANA ALVES DO PRADO")));
+		Mockito.when(randomGenerator.nextInt(6)).thenReturn(5, 0);
+		Collection<String> winners = drawWinner.buildWinner(DEFAULT_NAMES_LIST, 2);
+		MatcherAssert.assertThat(winners, Matchers.contains("ADRIANA ALVES DO PRADO", "ABNER MARCOS ORLAMUNDER"));
+		Mockito.verify(randomGenerator, Mockito.times(2)).nextInt(Mockito.anyInt());
 	}
 
 	@Test
@@ -40,14 +54,14 @@ public class DrawWinnerTest {
 	public void fiveWinnersDrawingFromFile() throws IOException {
 		List<String> nameList = fileReader.readFile("src/test/resources/com/softexpert/name-list-01.txt");
 		List<String> winners = drawWinner.buildWinner(nameList, 5);
-		
+
 		MatcherAssert.assertThat(winners.get(0), Matchers.isIn(nameList));
 		MatcherAssert.assertThat(winners.get(1), Matchers.isIn(nameList));
 		MatcherAssert.assertThat(winners.get(2), Matchers.isIn(nameList));
 		MatcherAssert.assertThat(winners.get(3), Matchers.isIn(nameList));
 		MatcherAssert.assertThat(winners.get(4), Matchers.isIn(nameList));
 	}
-	
+
 	@Test
 	public void fiveWinnersDrawingTwo() throws IOException {
 		List<String> nameList = fileReader.readFile("src/test/resources/com/softexpert/name-list-02.txt");
@@ -66,13 +80,13 @@ public class DrawWinnerTest {
 		Collection<String> winners = drawWinner.buildWinner(nameList, 100);
 		MatcherAssert.assertThat(winners, Matchers.hasSize(100));
 	}
-	
+
 	@Test
 	public void overThousandWinnersDrawing() throws IOException {
 		List<String> nameList = fileReader.readFile("src/test/resources/com/softexpert/name-list-03.txt");
 		Collection<String> winners = drawWinner.buildWinner(nameList, 1113);
 		MatcherAssert.assertThat(winners, Matchers.hasSize(1113));
-		
+
 	}
-	
+
 }
